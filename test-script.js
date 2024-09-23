e2f17f82f4b512e342c1f7448de6d08097d84393
dalytekab <113513082+dalytekab@users.noreply.github.com>
2024-09-23 15:57:55 +0100

    Update test-script.js

--- test-script.js
+++ test-script.js
@@ -1,6 +1,8 @@
 function callApi() {
   var url = "https://webhook.site/10fc0e6a-32e8-45c8-89fa-ae7c2a5aa317";
 
+  console.log("Before API call");
+
   fetch(url, {
     method: 'GET',
     mode: 'no-cors',
@@ -9,15 +11,12 @@ function callApi() {
     },
   })
   .then(function(response) {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(function(data) {
-    console.log('Success:', data);
+    console.log("After API call");
+    console.log("Response received:", response);
   })
   .catch(function(error) {
     console.error('Error:', error);
   });
+
+  console.log("API call initiated, not awaiting response");
 }
