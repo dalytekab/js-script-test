3a2b6b20320786622dafaf2cd7c684d5b9ef1268
dalytekab <113513082+dalytekab@users.noreply.github.com>
2024-09-23 16:04:01 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function callApi() {
+  var url = "https://webhook.site/10fc0e6a-32e8-45c8-89fa-ae7c2a5aa317";
+
+  console.log("Before API call");
+
+  fetch(url, {
+    method: 'GET',
+    mode: 'no-cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  .then(function(response) {
+    console.log("After API call");
+    console.log("Response received:", response);
+  })
+  .catch(function(error) {
+    console.error('Error:', error);
+  });
+
+  console.log("API call initiated, not awaiting response");
+}
