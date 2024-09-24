90fe53e28b58a15975cb1f58906e99f6f83148b1
dalytekab <113513082+dalytekab@users.noreply.github.com>
2024-09-24 16:34:51 +0100

    Create real-api-script.js

---
+++ real-api-script.js
@@ -0,0 +1,23 @@
+function callApi(codePromo) {
+  var url = "https://3000-flyingforyo-flyingforyo-ykf2e6b5xyg.ws-eu116.gitpod.io/stats&promo_code" + codePromo;
+
+  console.log("Before API call");
+
+  fetch(url, {
+    method: 'GET',
+    mode: 'no-cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  .then(function(response) {
+    console.log("After API call");
+    console.log("Response received:", response);
+  })
+  .catch(function(error) {
+    console.error('Error:', error);
+  });
+
+  console.log("API call initiated, not awaiting response");
+  return true
+}
